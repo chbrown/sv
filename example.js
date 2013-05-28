900d36503fbfbe3969fba461c126c49f4329a049
Christopher Brown <io@henrian.com>
2013-05-28 15:37:59 -0500

    Add example.js

---
+++ example.js
@@ -0,0 +1,57 @@
+var sv = require('./'); // require('sv') elsewhere
+
+function stringify() {
+  var expenses = [
+    {name: 'Tip'                },
+    {name: 'Lunch', amount: 5.90},
+    {name: 'Latte', amount: 3.15},
+    {name: 'Paper', amount: 2.10},
+    {name: 'Pens' , amount: 4.59},
+    {               amount: 9.16}
+  ];
+
+  var stringifier = new sv.Stringifier({peek: 2, missing: 'n/a'});
+  stringifier.pipe(process.stdout);
+  expenses.forEach(function(expense) {
+    stringifier.write(expense);
+  });
+  stringifier.end();
+}
+
+function parse() {
+  var doc = [
+    'name,amount',
+    'Tip,n/a',
+    'Lunch,5.9',
+    'Latte,3.15',
+    'Paper,2.1',
+    'Pens,4.59',
+    'n/a,9.16'
+  ].join('\n');
+
+  var parser = new sv.Parser({peek: 3});
+  parser.on('data', function(row) {
+    console.log(row);
+  });
+  parser.end(doc);
+}
+
+function stdin() {
+  var parser = new sv.Parser();
+  // logEvents(parser, 'parser', ['finish', 'close', 'drain', 'error']);
+  // logEvents(process.stdout, 'process.stdout', ['end', 'finish', 'close', 'drain', 'error']);
+  // process.stdout.on('error', function(row) {
+  //   console.log('error', row);
+  // });
+  parser.on('data', function(row) {
+    console.log(row);
+  });
+  // parser.pipe(process.stdout);
+  process.stdin.pipe(parser);
+}
+
+console.log("stringify example");
+stringify();
+
+console.log("parse example");
+parse();
