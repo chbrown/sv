a7b7ea1321730208dc98503da3bd2cb00cf3650a
Christopher Brown <io@henrian.com>
2016-05-01 09:20:51 -0500

    Removed compiled files from source control

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Parser } from './parser';
-import { Stringifier } from './stringifier';
-export { Parser, Stringifier };
-export declare function transform(input: NodeJS.ReadableStream, parserConfig: any, stringifierConfig: any, callback: any): void;
-export declare function main(): void;
